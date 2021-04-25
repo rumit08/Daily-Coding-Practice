95d7f9115d2200c92fb0cb389730e2c49f8fe3e3
rumit08 <rumit.kalavadiya@gmail.com>
2021-04-25 14:25:09 +0530

    Apr_24_25 | StrongPassword_Pangrams

---
+++ Apr_24.java
@@ -0,0 +1,24 @@
+package daily.coding.practice;
+
+import java.util.regex.Pattern;
+
+/*https://www.hackerrank.com/challenges/strong-password/problem*/
+public class Apr_24 {
+	public static void main(String arg[]){
+		String s="#HackerRank";
+		int n=s.length();
+		int count=0;
+        if(Pattern.matches("[^a-z]+", s)) count++;
+        if(Pattern.matches("[^A-Z]+", s)) count++;
+        if(Pattern.matches("[^0-9]+", s)) count++;
+        if(Pattern.matches("[^!@#$%^&*()+-]+", s)) count++;
+        if(n>=6)
+        	System.out.println(count);
+        else{
+            int tmp=6-(n+count);
+            if(tmp>0) System.out.println(count+tmp);
+            else System.out.println(count); 
+        }
+		
+	}
+}
---
+++ Apr_25.java
@@ -0,0 +1,19 @@
+package daily.coding.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Apr_25 {
+	public static void main(String arg[]){
+		String s="We promptly judged antique ivory buckles for the next prize";
+		String tmp=s.toLowerCase();
+		System.out.println(tmp);
+		Map<Character,Integer> map=new HashMap<>();
+		for(int i=0;i<tmp.length();i++){
+			if(tmp.charAt(i)!=' ' && !map.containsKey(tmp.charAt(i))){
+				map.put(tmp.charAt(i),1);
+			}
+		}
+		System.out.println(map.size());
+	}
+}
